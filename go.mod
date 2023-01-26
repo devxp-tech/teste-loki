9628d3b46fe593cf273049a38a597274d4433634
Diego Luisi <dluisi@evolution.com>
2023-01-26 14:17:43 +0000

    [0.1.55] :zap: Add Commands

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/devxp-tech/teste-loki
 go 1.18
 
 require (
-	github.com/getsentry/sentry-go v0.17.0
 	github.com/gin-gonic/gin v1.8.1
 	github.com/gritzkoo/golang-health-checker-lw v1.0.1
 	github.com/sirupsen/logrus v1.9.0
@@ -15,6 +14,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.1 // indirect
 	github.com/goccy/go-json v0.9.11 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
--- main.go
+++ main.go
@@ -1,31 +1,12 @@
 package main
 
 import (
-	"log"
-	"time"
-
 	_ "github.com/devxp-tech/teste-loki/config"
 	"github.com/devxp-tech/teste-loki/controllers"
-	"github.com/getsentry/sentry-go"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-
-	err := sentry.Init(sentry.ClientOptions{
-		Dsn: "https://3f0ed90a9758404e89028367d40759a6@o4504350064836608.ingest.sentry.io/4504570239975424",
-		// Set TracesSampleRate to 1.0 to capture 100%
-		// of transactions for performance monitoring.
-		// We recommend adjusting this value in production,
-		TracesSampleRate: 1.0,
-	})
-	if err != nil {
-		log.Fatalf("sentry.Init: %s", err)
-	}
-	// Flush buffered events before the program terminates.
-	defer sentry.Flush(2 * time.Second)
-
-	sentry.CaptureMessage("It works!")
 
 	server := gin.New()
 	server.GET("/", func(c *gin.Context) {
