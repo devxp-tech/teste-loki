a277cc7ddaf13d7b1bdd148a5f305c0d6eec4192
Diego Luisi <dluisi@evolution.com>
2023-03-28 13:39:45 +0100

    :zap: Upgraded from release 1.12.0 to 1.12.1

--- main.go
+++ main.go
@@ -1,13 +1,34 @@
 package main
 
 import (
+	"time"
+
 	_ "github.com/devxp-tech/teste-loki/config"
 	"github.com/devxp-tech/teste-loki/controllers"
 	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+func recordMetrics() {
+	go func() {
+		for {
+			opsProcessed.Inc()
+			time.Sleep(2 * time.Second)
+		}
+	}()
+}
+
+var (
+	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "myapp_processed_ops_total",
+		Help: "The total number of processed events",
+	})
 )
 
 func main() {
-
+	recordMetrics()
 	server := gin.New()
 	server.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
@@ -16,6 +37,12 @@ func main() {
 			"headers": c.Request.Header,
 		})
 	})
+
+	server.GET("/metrics", func(c *gin.Context) {
+		h := promhttp.Handler()
+		h.ServeHTTP(c.Writer, c.Request)
+	})
+
 	server.GET("/health-check/liveness", controllers.HealthCheckLiveness)
 	server.GET("/health-check/readiness", controllers.HealthCheckReadiness)
 
